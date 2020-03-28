f8eabac1b51a3eb61aecb79cdcaf7c7230f307d1
sackey24 <sackeyad24@gmail.com>
2020-03-28 14:40:09 +0900

    Add abc102c

--- abc102/abc102_c.cpp
+++ abc102/abc102_c.cpp
@@ -2,4 +2,23 @@
 using namespace std;
 
 int main(){
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++)
+  {
+    cin >> a[i];
+    a[i] -= i+1;
+  }
+  sort(a.begin(),a.end());
+  int m = a[a.size()/2];
+
+  long long ans = 0;
+  for (int i = 0; i < n; i++)
+  {
+    a[i]-=m;
+    a[i] = abs(a[i]);
+    ans+=a[i];
+  }
+  cout << ans << endl;
 }
