39a2be05fd907a9ef9786c8fbf7ba5139551bdbb
sackey24 <sackeyad24@gmail.com>
2020-03-31 23:35:20 +0900

    abc104b

--- abc104/abc104_b.cpp
+++ abc104/abc104_b.cpp
@@ -2,4 +2,24 @@
 using namespace std;
 
 int main(){
+  string s;
+  cin >> s;
+  int n = s.size();
+  bool ok = true;
+
+  if(s[0]!='A') ok = false;
+  int count = 0;
+  for (int i = 2; i < n-1; i++)
+  {
+    if(s[i]=='C') count++;
+  }
+  if(count!=1) ok = false;
+
+  for (int i = 0; i < n; i++)
+  {
+    if(s[i]<'a' && s[i]!='A' && s[i]!='C') ok = false;
+  }
+
+  if(ok) puts("AC");
+  else puts("WA");
 }
