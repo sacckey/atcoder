d4a5250028ab7e70f51ee3e1c854c9833424c44b
sackey24 <sackeyad24@gmail.com>
2020-04-22 07:41:00 +0900

    abc160d-e

--- abc160/abc160_d.cpp
+++ abc160/abc160_d.cpp
@@ -1,5 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<vector<int>> g;
+int n;
+
+void bfs(int node,vector<vector<int>> &diff){
+  queue<pair<int,int>> que;
+  que.emplace(node,0);
+  vector<int> seen(n);
+  seen[node]=1;
+
+  while(!que.empty()){
+    int now = que.front().first;
+    int cost = que.front().second;
+    que.pop();
+
+    for(int next:g[now]){
+      if(seen[next]) continue;
+      diff[node][next] = cost+1;
+      que.emplace(next,cost+1);
+      seen[next]=1;
+    }
+  } 
+}
+
 int main(){
+  int x,y;
+  cin >> n >> x >> y;
+  g.assign(n,vector<int>());
+  x--;
+  y--;
+
+  for (int i = 0; i < n-1; i++)
+  {
+    g[i].push_back(i+1);
+    g[i+1].push_back(i);
+  }
+  g[x].push_back(y);
+  g[y].push_back(x);
+
+  const int INF = 1e9;
+  vector<vector<int>> diff(n,vector<int>(n,INF));
+  for (int i = 0; i < n; i++)
+  {
+    bfs(i,diff);
+  }
+
+  vector<int> anss(n);
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = i+1; j < n; j++)
+    {
+      if(diff[i][j]<n) anss[diff[i][j]]++;
+    }
+  }
+
+  for (int i = 1; i < n; i++)
+  {
+    cout << anss[i] << endl;
+  }  
 }
--- abc160/abc160_e.cpp
+++ abc160/abc160_e.cpp
@@ -2,4 +2,45 @@
 using namespace std;
 
 int main(){
+  int x,y,a,b,c;
+  cin >> x >> y >> a >> b >> c;
+  vector<int> p(a),q(b);
+  priority_queue<int> r;
+  for (int i = 0; i < a; i++) cin >> p[i];
+  for (int i = 0; i < b; i++) cin >> q[i];
+  for (int i = 0; i < c; i++){
+    int ri;
+    cin >> ri;
+    r.push(ri);
+  }
+
+  sort(p.begin(),p.end());
+  sort(q.begin(),q.end());
+  reverse(p.begin(),p.end());
+  reverse(q.begin(),q.end());
+
+  vector<int> anss;
+  long long ans = 0;
+  for (int i = 0; i < x; i++){
+    anss.push_back(p[i]);
+    ans+=p[i];
+  }
+  for (int i = 0; i < y; i++){
+    anss.push_back(q[i]);
+    ans+=q[i];
+  }
+  sort(anss.begin(),anss.end());
+  
+  for (int i = 0; i < x+y; i++)
+  {
+    if(r.empty()) break;
+    int mu = r.top();
+    r.pop();
+    if(anss[i]<mu){
+      ans-=anss[i];
+      ans+=mu;
+    }
+    else break;
+  }
+  cout << ans << endl;  
 }
